fa4991bb31ed50b23b88905fb531087ed0a5cc79
Jon <jon.a.mccallum@gmail.com>
2022-01-11 07:33:44 +0000

    practice use of v-html directive

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const app = Vue.createApp({
   data() {
     return {
-      goal: 'finish the course',
+      goal: 'goal a',
+      goalB: '<h2>goal b</h2>',
+      vueLink: 'https://vuejs.org',
     };
+  },
+  methods: {
+    outPutGoal() {
+      const rand = Math.random();
+      return rand > 0.5 ? this.goal : this.goalB;
+    },
   },
 });
 
